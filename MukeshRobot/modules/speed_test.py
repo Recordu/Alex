ea5cca4a7f6a66537dac95ebd160e9d52ccf3d15
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-05-19 20:02:50 +0530

    Update speed_test.py

--- MukeshRobot/modules/speed_test.py
+++ MukeshRobot/modules/speed_test.py
@@ -63,9 +63,9 @@ dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 __help__ = """
 » /speedtest *:* ʀᴜɴs ᴀ sᴘᴇᴇᴅᴛᴇsᴛ ᴀɴᴅ ᴄʜᴇᴄᴋ ᴛʜᴇ sᴇʀᴠᴇʀ sᴘᴇᴇᴅ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+
 """
 
-__mod_name__ = "⚡SᴘᴇᴇᴅTᴇsᴛ⚡​"
+__mod_name__ = "𝐒ᴘᴇᴇᴅTᴇsᴛ"
 __command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
