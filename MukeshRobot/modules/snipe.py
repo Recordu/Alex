0262b40312f0cd38abda21049f0f0cf0460ca191
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-05-19 20:12:00 +0530

    Update snipe.py

--- MukeshRobot/modules/snipe.py
+++ MukeshRobot/modules/snipe.py
@@ -31,10 +31,10 @@ __help__ = """
 • /snipe <ᴄʜᴀᴛɪᴅ> <sᴛʀɪɴɢ>
 ᴍᴀᴋᴇ ᴍᴇ sᴇɴᴅ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴀ sᴘᴇᴄɪғɪᴄ ᴄʜᴀᴛ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+
 """
 
-__mod_name__ = "⚡sɴɪᴘᴇ⚡"
+__mod_name__ = "𝐒ɴɪᴘᴇ"
 
 SNIPE_HANDLER = CommandHandler(
     "snipe", snipe, pass_args=True, filters=CustomFilters.dev_filter
