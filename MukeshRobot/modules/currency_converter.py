9c86fe17423be785f403bd864e97310d0285da3b
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-05-20 13:08:57 +0530

    Update currency_converter.py

--- MukeshRobot/modules/currency_converter.py
+++ MukeshRobot/modules/currency_converter.py
@@ -57,9 +57,9 @@ dispatcher.add_handler(CONVERTER_HANDLER)
 
 __command_list__ = ["cash"]
 __handlers__ = [CONVERTER_HANDLER]
-__mod_name__ = "♨️ᴄᴀsʜ♨️"
+__mod_name__ = "𝐂ᴀsʜ"
 __help__ = """
- /ᴄᴀsʜ  ᴄᴏɴᴠᴇʀᴛ ᴇᴀᴄʜ ᴏᴛʜᴇʀ ᴄᴜʀʀᴇɴᴄʏ 
- /ᴄᴏɴᴠᴇʀᴛ ᴏʀ ᴛʀʏ 
+ /cash  ᴄᴏɴᴠᴇʀᴛ ᴇᴀᴄʜ ᴏᴛʜᴇʀ ᴄᴜʀʀᴇɴᴄʏ 
+ /convert ᴏʀ ᴛʀʏ 
  
- ☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆"""
+"""
