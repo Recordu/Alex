b334803a5759c2afcef223bd8c10db70a4d934be
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-05-20 13:32:12 +0530

    Update animez.py

--- MukeshRobot/modules/animez.py
+++ MukeshRobot/modules/animez.py
@@ -243,8 +243,8 @@ def anime(update: Update, context: CallbackContext):
         if trailer:
             buttons = [
                 [
-                    InlineKeyboardButton("📍ᴍᴏʀᴇ ɪɴғᴏ📍", url=info),
-                    InlineKeyboardButton("📍ᴛʀᴀɪʟᴇʀ📍", url=trailer),
+                    InlineKeyboardButton("ᴍᴏʀᴇ ɪɴғᴏ", url=info),
+                    InlineKeyboardButton("ᴛʀᴀɪʟᴇʀ", url=trailer),
                 ]
             ]
         else:
@@ -554,7 +554,7 @@ __help__ = """
  ❍ /kayo <ᴀɴɪᴍᴇ>*:* sᴇᴀʀᴄʜ ᴀɴ ᴀɴɪᴍᴇ ᴏɴ ᴀɴɪᴍᴇᴋᴀʏᴏ.ᴄᴏᴍ
  ❍ /airing <ᴀɴɪᴍᴇ>*:* ʀᴇᴛᴜʀɴs ᴀɴɪᴍᴇ ᴀɪʀɪɴɢ ɪɴғᴏ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+
 """
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime)
@@ -575,7 +575,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "♨️Aɴɪᴍᴇ♨️"
+__mod_name__ = "𝐀ɴɪᴍᴇ"
 __command_list__ = [
     "anime",
     "manga",
